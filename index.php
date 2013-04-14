8e25629de19084d4f3bdd43ce43e328a05144785
omar <omar@lt.(none)>
2013-04-13 23:17:19 -0500

    agregndo cargador permanente

--- index.php
+++ index.php
@@ -9,7 +9,28 @@
 	<meta charset="UTF-8">
 	<title>Miiluma | Bienvenido</title>
 </head>
+	
+	<div class="load_images">
+		
+	<div src="" alt="" class="main inicio"></div>
+	<div src="" alt="" class="main bienvenida"></div>
+	<div src="" alt="" class="main contacto"></div>
+	<div src="" alt="" class="main cotizacion"></div>
+	<div src="" alt="" class="main instalaciones"></div>
+	<div src="" alt="" class="main servicios"></div>
+	<div src="" alt="" class="main paquetes"></div>
 
+
+
+
+
+
+
+
+
+
+	</div>
+	
 	<?php include "assets.php"; ?>
 	<link rel="stylesheet" href="css/normalize.css"/>
 	<link rel="stylesheet" href="css/style.css"/>
