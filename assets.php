12f7633ab8921e995e227e98b8aef83d6cb9190f
omar <omar@lt.(none)>
2013-04-13 23:43:49 -0500

    headers

--- assets.php
+++ assets.php
@@ -1,3 +1,13 @@
+
+<meta charset="UTF-8">
+<meta http-equiv="Expires" content="0">
+
+<meta http-equiv="Last-Modified" content="0">
+
+<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+
+<meta http-equiv="Pragma" content="no-cache">
+
 	<script src="js/jquery.js"></script>
 	<script src="js/jloader.js"></script>
 	<script src="js/init_loader.js"></script>
