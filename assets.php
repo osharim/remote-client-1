dbd4ed02b43aceb7735ed3e5a40bfd40e1af25e6
omar <omar@lt.(none)>
2013-04-15 13:53:47 -0500

    agregando tracker

--- assets.php
+++ assets.php
@@ -11,6 +11,17 @@ $app_version = '1.1';
 <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 <![endif]–> 
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40104766-1', 'herokuapp.com');
+  ga('send', 'pageview');
+
+</script>
+
 
 	<script src="js/jquery.js<?php include 'version.php' ?>"></script>
 		<script src="js/modernizr.js<?php include 'version.php' ?>"></script>
